1b098d23b94df9c37d8db383f74345ec100d2cef
jandedeurwaerder-howest <jan.de.deurwaerder@howest.be>
2022-05-31 13:43:00 +0200

    Add project files.

---
+++ Scala.Vergaderingen.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Scala.Vergaderingen.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        bool isNew;
+        private void BouwUren()
+        {
+
+        }
+        private void BeeldStandaard()
+        {
+
+        }
+        private void BeeldBewerken()
+        {
+
+        }
+        private void ClearControls()
+        {
+
+        }
+        private void PopulateVergaderingen()
+        {
+        }
+        private void PopulatePersonen()
+        {
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void LstVergaderingen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void DtpFilter_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void BtnFilterWissen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnNiew_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnWijzig_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnAnnuleren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBewaren_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnPersoonToevoegen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnPersoonVerwijderen_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
